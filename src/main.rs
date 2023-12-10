62bde2fc5e51d18b3b6917dd409277733e606b8a
Maddi <16472317+MaddiFurr@users.noreply.github.com>
2023-12-10 01:24:54 -0600

    Added Console logging output via the diagnostics plugin. https://docs.rs/bevy/latest/bevy/diagnostic/index.html

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,41 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, ecs::query, diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}};
 
+#[derive(Component, Debug)]
+struct Position {
+    x: f32,
+    y: f32,
+}
 
-#[derive(Component)]
-struct Position {x : f32, y: f32}
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
+#[derive(Component, Debug)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
 
 fn main() {
     App::new()
+        //.add_systems(Startup, spawn_spaceship)
+        .add_systems(Update,(spawn_spaceship, update_position))
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, tartup)
-        .add_systems(Update, print_position_system)
-        .add_systems(Startup, greetings)
+        .add_plugins(FrameTimeDiagnosticsPlugin::default())
+        .add_plugins(LogDiagnosticsPlugin::default())
         .run();
-    
 }
 
-fn tartup(mut commands: Commands) {
-    println!("Hello World");
-    println!("woo");
-   
-    commands.spawn((Person, Name("Person 1".to_string())));
-
-    commands.spawn((Person, Name("Furry 1".to_string())));
+fn spawn_spaceship(mut commands: Commands) {
+    commands.spawn((Position {x:0.0, y: 0.0 }, Velocity {x: 1.0, y: 1.0 }));
 }
 
-
-fn print_position_system(query: Query<&Position>) {
-    for position in &query {
-        println!("Position: {} {}", position.x, position.y)
+fn update_position(mut query: Query<(&Velocity, &mut Position)>) {
+    for (v, mut p) in query.iter_mut() {
+        p.x += v.x;
+        p.y += v.y;
     }
 }
 
-
-fn greetings (query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hello there {}!", name.0)
-
+fn print_position(query: Query<(Entity, &Position)>) {
+    // Log the entity ID and position of each with a 'Position' component
+    for (entity, position) in query.iter() {
+        info!("Entity: {:?} is at position {:?},", entity, position)
     }
 }
