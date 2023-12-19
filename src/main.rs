adabb470e48ff68c5d0720e9e7d876cc0fd2b6cc
Maddi <dawhat18@gmail.com>
2023-12-19 02:56:49 -0600

    Started implimenting some basic plugins

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,53 @@
-use bevy::{prelude::*, ecs::query, diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}};
+use bevy::{prelude::*, diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}};
+
+mod initializers;
 
 #[derive(Component, Debug)]
-struct Position {
-    x: f32,
-    y: f32,
+pub struct Velocity {
+    pub value: Vec3,
 }
 
-#[derive(Component, Debug)]
-struct Velocity {
-    x: f32,
-    y: f32,
+pub struct SpaceshipPlugin;
+
+impl Plugin for SpaceshipPlugin {
+    fn build(&self, app:&mut App) {
+        app.add_systems(Startup, spawn_spaceship);
+    }
 }
 
 fn main() {
     App::new()
-        //.add_systems(Startup, spawn_spaceship)
-        .add_systems(Update,(spawn_spaceship, update_position))
+        .add_systems(Startup, spawn_spaceship)
+        .add_systems(Update,(update_position, print_position))
         .add_plugins(DefaultPlugins)
         .add_plugins(FrameTimeDiagnosticsPlugin::default())
         .add_plugins(LogDiagnosticsPlugin::default())
         .run();
 }
 
-fn spawn_spaceship(mut commands: Commands) {
-    commands.spawn((Position {x:0.0, y: 0.0 }, Velocity {x: 1.0, y: 1.0 }));
+fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        SpatialBundle::default(),
+        Velocity {
+            value: Vec3::new(0.,0., 0.),
+        },
+    ));
 }
 
-fn update_position(mut query: Query<(&Velocity, &mut Position)>) {
-    for (v, mut p) in query.iter_mut() {
-        p.x += v.x;
-        p.y += v.y;
+fn update_position(mut query: Query<(&Velocity, &mut Transform)>) {
+    for (velocity, mut transform) in query.iter_mut() {
+        transform.translation.x += velocity.value.x;
+        transform.translation.y += velocity.value.y;
+        transform.translation.z += velocity.value.z;
     }
 }
 
-fn print_position(query: Query<(Entity, &Position)>) {
+fn print_position(query: Query<(Entity, &Transform)>) {
     // Log the entity ID and position of each with a 'Position' component
-    for (entity, position) in query.iter() {
-        info!("Entity: {:?} is at position {:?},", entity, position)
+    for (entity, transform) in query.iter() {
+        info!(
+            "Entity {:?} is at position {:?},",
+            entity, transform.translation
+        )
     }
 }
