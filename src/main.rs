6b17ee120c8e4850375bce0b5ac1e54df6cc6796
Maddi <dawhat18@gmail.com>
2023-12-21 03:39:29 -0600

    it now has a spaceship :3

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,25 @@
+mod debug;
+mod movement;
+mod spaceships;
+mod camera;
+
 use bevy::{prelude::*, diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}};
-
-mod initializers;
-
-#[derive(Component, Debug)]
-pub struct Velocity {
-    pub value: Vec3,
-}
-
-pub struct SpaceshipPlugin;
-
-impl Plugin for SpaceshipPlugin {
-    fn build(&self, app:&mut App) {
-        app.add_systems(Startup, spawn_spaceship);
-    }
-}
+use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceships::SpaceshipPlugin;
+use camera::CameraPlugin;
 
 fn main() {
     App::new()
-        .add_systems(Startup, spawn_spaceship)
-        .add_systems(Update,(update_position, print_position))
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.00, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.75,
+        })
         .add_plugins(DefaultPlugins)
-        .add_plugins(FrameTimeDiagnosticsPlugin::default())
-        .add_plugins(LogDiagnosticsPlugin::default())
+        .add_plugins(DebugPlugin)
+        .add_plugins(MovementPlugin)
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(CameraPlugin)
         .run();
-}
-
-fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((
-        SpatialBundle::default(),
-        Velocity {
-            value: Vec3::new(0.,0., 0.),
-        },
-    ));
-}
-
-fn update_position(mut query: Query<(&Velocity, &mut Transform)>) {
-    for (velocity, mut transform) in query.iter_mut() {
-        transform.translation.x += velocity.value.x;
-        transform.translation.y += velocity.value.y;
-        transform.translation.z += velocity.value.z;
-    }
-}
-
-fn print_position(query: Query<(Entity, &Transform)>) {
-    // Log the entity ID and position of each with a 'Position' component
-    for (entity, transform) in query.iter() {
-        info!(
-            "Entity {:?} is at position {:?},",
-            entity, transform.translation
-        )
-    }
 }
